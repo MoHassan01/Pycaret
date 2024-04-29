3bb572d2cbf37874ce4f10d9b47df5d6e3d134b5
“MoHassan01” <moohassan01@gmail.com>
2024-04-29 13:18:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import LabelEncoder
 import numpy as np
 
 st.subheader("File upload")
@@ -29,40 +29,40 @@ if file:
         ["Remove them", "Fill with a specific value", "Impute them"],
     )
 
+    # Detecting numerical columns
+    num_cols = df.select_dtypes(exclude="object").columns.tolist()
+
     if radio == "Remove them":
         df.dropna()
         st.write("Data new shape:", df.shape)
-    if radio == "Fill with a specific value":
+    elif radio == "Fill with a specific value":
         val = st.text_input("Value")
-        df.fillna(val, inplace=True)
+        df[num_cols].fillna(val, inplace=True)
+    elif radio == "Impute them":
+        df[num_cols] = df[num_cols].interpolate(
+            method="linear", limit_direction="forward"
+        )
 
     st.subheader("Choose y-column")
     y = st.selectbox("Select y", df.columns)
 
     # Data Encoding
     st.subheader("Categorical data encoding")
+
     # Detecting categorical columns
     cat_cols = df.select_dtypes(include=["object"]).columns.to_list()
 
     enc = st.radio("Select encoder", ["Label encoder", "One-Hot encoder"])
 
     if enc == "One-Hot encoder":
-        encoder = OneHotEncoder()
-        enc_arr = encoder.fit_transform(df[cat_cols])
-        enc_df = pd.DataFrame(enc_arr, index=df.index)
-        other_cols = df.drop(columns=cat_cols)
-        df_encoded = pd.concat([enc_df, other_cols], axis=1)
-        df = df_encoded
-        st.dataframe(df)
+        df = pd.get_dummies(df, columns=cat_cols)
+        st.write(df.shape)
+        st.dataframe(df.sample(10))
 
     if enc == "Label encoder":
-        encoder = LabelEncoder()
-        enc_arr = encoder.fit_transform(df[cat_cols])
-        enc_df = pd.DataFrame(enc_arr, index=df.index)
-        other_cols = df.drop(columns=cat_cols)
-        df_encoded = pd.concat([enc_df, other_cols], axis=1)
-        df = df_encoded
-        st.dataframe(df)
+        for col in cat_cols:
+            df[col] = LabelEncoder().fit_transform(df[col])
+        st.dataframe(df.sample(10))
 
     # Detect classificatoion or regression
     if df[y].nunique() == 2:
